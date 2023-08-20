f104695f9c56a0a99b5ced5225d2131b9c3d9c6c
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-08-19 22:43:09 -0400

    Time: 42 ms (86.25%), Space: 16.3 MB (54.78%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        # from last index of nums1 to insert new val
         last_idx = m + n - 1
 
         while m > 0 and n > 0:
@@ -18,19 +19,4 @@ class Solution:
             nums1[last_idx] = nums2[n-1]
             last_idx -= 1; n -= 1
         return nums1
-
-
-        # last_idx = m + n - 1
-        # while m > 0 and n > 0:
-        #     if nums1[m - 1] > nums2[n - 1]:
-        #         nums1[last_idx] = nums1[m - 1]
-        #         m -= 1
-        #     else:
-        #         nums1[last_idx] = nums2[n - 1]
-        #         n -= 1
-        #     last_idx -= 1
-        
-        # while n > 0:
-        #     nums1[last_idx] = nums2[n - 1]
-        #     n -= 1; last_idx -= 1
         
