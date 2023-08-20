ea882689cbcac74336e528cf599cd8224a923e10
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-08-20 18:24:11 -0400

    Time: 52 ms (48.29%), Space: 16.9 MB (16.47%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True)  # Sort the citations in non-ascending order
+        h_index = 0
+    
+        for i, citation in enumerate(citations, start=1):
+            print(citation, i)
+            if citation >= i:
+                h_index = i  # Update the h-index if condition is met
+            else:
+                break  # Stop the loop when condition is not met
+    
+        return h_index
