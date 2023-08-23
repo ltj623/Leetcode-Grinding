9994ca70c6396f2211bc1508c24344f656f6c99f
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-08-23 18:59:52 -0400

    Time: 47 ms (42.11%), Space: 16.4 MB (23.65%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        l = 0
+        if not s: return True
+        for i, c in enumerate(t):
+            if c == s[l]:
+                l += 1
+                if l == len(s): return True
+        return False
+        # l = 0
+        # if not s: return True
+
+        # for r in range(len(t)):
+        #     if s[l] == t[r]:
+        #         l += 1
+        #     if l == len(s): return True
+        # return False
