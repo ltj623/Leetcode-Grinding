10b371ae3143e665d7ac9809cf4a007a91771495
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-08-23 19:00:10 -0400

    Time: 38 ms (83.38%), Space: 16.2 MB (89.31%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -5,7 +5,7 @@ class Solution:
         for i, c in enumerate(t):
             if c == s[l]:
                 l += 1
-                if l == len(s): return True
+            if l == len(s): return True
         return False
         # l = 0
         # if not s: return True
