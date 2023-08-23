6cc25b72376e5a710558bfd932e66ce0b36c7de5
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-08-23 16:58:54 -0400

    Time: 72 ms (47.34%), Space: 16.2 MB (96.74%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -7,6 +7,6 @@ class Solution:
             increment = (numRows - 1) * 2
             for i in range(r, len(s), increment):
                 res += s[i]
-                if (r > 0 and r < numRows - 1 and i + increment - 2 * r < len(s)):
+                if 0 < r < numRows - 1 and i + increment - 2 * r < len(s):
                     res += s[i + increment - 2 * r]
         return res
