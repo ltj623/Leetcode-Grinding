4bf977340f55d5d18d68c7b8763140fd27d8eb16
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-08-23 19:00:51 -0400

    Time: 39 ms (78.77%), Space: 16.5 MB (23.65%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,17 +1,10 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         l = 0
+        # edge case, important, will cause out of index
         if not s: return True
         for i, c in enumerate(t):
             if c == s[l]:
                 l += 1
             if l == len(s): return True
         return False
-        # l = 0
-        # if not s: return True
-
-        # for r in range(len(t)):
-        #     if s[l] == t[r]:
-        #         l += 1
-        #     if l == len(s): return True
-        # return False
