eaf2ef579114010513b5fd4283e5a87360bf3f1e
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-08-24 17:16:20 -0400

    Time: 48 ms (93.79%), Space: 16.5 MB (63.72%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,8 +1,6 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        if len(ransomNote) > len(magazine): return False
-        cnt_r, cnt_m = Counter(ransomNote), Counter(magazine)
-
-        for c in ransomNote:
-            if cnt_r[c] > cnt_m[c]: return False
+        for c in set(ransomNote):
+            if ransomNote.count(c) > magazine.count(c):
+                return False
         return True
