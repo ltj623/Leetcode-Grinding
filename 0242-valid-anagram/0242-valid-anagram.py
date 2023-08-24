a4108b330c08fbc4aaec8ed756f8b0b68f937026
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-08-24 18:45:34 -0400

    Time: 67 ms (34.67%), Space: 16.9 MB (61.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -8,19 +8,4 @@ class Solution:
             if cnt_s[c] == 0: del cnt_s[c]
         return True if not cnt_s else False
 
-
-
-
-        h = {}
-        for c in s:
-            h[c] = h.get(c,0) + 1
-        for c in t:
-            if c not in h:
-                return False
-            else:
-                h[c] -= 1
-                if h[c] == 0:
-                    del h[c]
-        return True if not h else False
-
         
