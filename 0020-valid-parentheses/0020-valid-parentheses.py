0ca75d9e220d5355c72e8341c614e141e1800f1b
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-08-26 20:28:44 -0400

    Time: 31 ms (95.93%), Space: 16.3 MB (51.13%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        dict = {")":"(","}":"{","]":"["}
+
+        for c in s:
+            if c in dict:
+                if stack and stack[-1] == dict[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False
+        
