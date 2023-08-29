ebbd6c54c40e8c2a693ce9a66ee148fa1e479ea1
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-08-29 17:00:10 -0400

    Time: 71 ms (80.67%), Space: 17.6 MB (87.34%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -4,22 +4,20 @@ class Solution:
         sign, stack = 1, []
 
         for c in s:
-            if c.isdigit():
-                num = num*10 + int(c)
+            if c.isdigit(): num = num * 10 + int(c)
             elif c in '+-':
                 res += num * sign
-                sign = 1 if c == '+' else -1
+                sign = -1 if c == '-' else 1
                 num = 0
             elif c == '(':
                 stack.append(res)
                 stack.append(sign)
-                res, sign = 0, 1
+                sign = 1
+                res = 0
             elif c == ')':
                 res += num * sign
                 res *= stack.pop()
                 res += stack.pop()
                 num = 0
         return res + num * sign
-
-
         
