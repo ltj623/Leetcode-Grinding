005346511b11c7da0bb3ef66b8511d2ae536b1bc
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-08-31 13:02:34 -0400

    Time: 70 ms (28.85%), Space: 16.4 MB (51.51%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,13 +1,12 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         l, r = 0, 0
+
         while r < len(nums):
             count = 1
-            # Count how many same numbers with right pointer
             while r + 1 < len(nums) and nums[r] == nums[r+1]:
                 r += 1
                 count += 1
-            # the maximum times for a number is twice, so move the left pointer
             for _ in range(min(2, count)):
                 nums[l] = nums[r]
                 l += 1
