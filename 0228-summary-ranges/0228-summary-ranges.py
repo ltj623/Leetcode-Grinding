fd8ae30495b58962068f2e6d409ed31bdc8abe2e
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-09-02 19:32:14 -0400

    Time: 36 ms (80.53%), Space: 16.3 MB (35.69%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -1,9 +1,9 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
+        l = 0
         res = []
-        l, r = 0, 0
 
-        while l < len(nums) and r < len(nums):
+        for r in range(len(nums)):
             if r + 1 < len(nums) and nums[r + 1] == nums[r] + 1:
                 r += 1
             else:
@@ -15,5 +15,21 @@ class Solution:
                     r += 1
                     l = r
         return res
+
+        # res = []
+        # l, r = 0, 0
+
+        # while l < len(nums) and r < len(nums):
+        #     if r + 1 < len(nums) and nums[r + 1] == nums[r] + 1:
+        #         r += 1
+        #     else:
+        #         if nums[l] == nums[r]:
+        #             res.append(str(nums[l]))
+        #             l += 1; r += 1
+        #         else:
+        #             res.append(str(nums[l]) + '->' + str(nums[r]))
+        #             r += 1
+        #             l = r
+        # return res
 
 
