6e6b9bc0120c163a7361a8d0b0f06235bcf4b1c7
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-09-03 09:33:02 -0400

    Time: 41 ms (67.79%), Space: 16.2 MB (99.26%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -4,13 +4,15 @@ class Solution:
         cur = ""
 
         for c in path + '/':
-            if c == '/':
+            if c == "/":
                 if cur == "..":
                     if stack: stack.pop()
+                # skip if cur == "" or cur == "." since its meaningless
                 elif cur != "" and cur != ".":
                     stack.append(cur)
+                # initiate cur
                 cur = ""
             else:
                 cur += c
         return '/' + '/'.join(stack)
-
+        
