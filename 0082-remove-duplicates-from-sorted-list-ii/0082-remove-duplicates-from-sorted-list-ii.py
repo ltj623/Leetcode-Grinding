9fba4d3614ef337e3e119279239640f5e0fe8da7
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-09-06 13:32:38 -0400

    Time: 50 ms (48.17%), Space: 16.3 MB (53.91%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -10,11 +10,13 @@ class Solution:
         curr = head
 
         while curr:
+            # check if curr node is duplicate node
             if curr.next and curr.next.val == curr.val:
                 while curr.next and curr.next.val == curr.val:
                     curr = curr.next
                 prev.next = curr.next
             else:
+                # if not duplicate, move prev and curr to next nodes
                 prev = prev.next
             curr = curr.next
         return dummy.next
