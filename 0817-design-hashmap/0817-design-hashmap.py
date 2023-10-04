26fa263c94f2696f9e4337097828ea9ef824f3ce
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-10-04 19:33:35 -0400

    Time: 194 ms (78.36%), Space: 20.4 MB (29.16%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.py
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -6,7 +6,7 @@ class ListNode:
 class MyHashMap:
 
     def __init__(self):
-        self.size = 1000
+        self.size = 10000
         self.hash_table = [None] * self.size 
 
     def put(self, key: int, value: int) -> None:
