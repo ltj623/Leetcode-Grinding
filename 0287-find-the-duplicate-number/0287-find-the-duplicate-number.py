1d367510678f28b087816ca56644713e16802326
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-10-04 11:44:22 -0400

    Time: 497 ms (95.49%), Space: 31 MB (45.71%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,10 +1,10 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         freq = [0] * len(nums)
+
         for num in nums:
             if freq[num]:
                 return num
             freq[num] = 1
-        
 
         
