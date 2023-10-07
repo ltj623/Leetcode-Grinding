5c76162b0f8088f4521fdba1fbddbbc02a51d118
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-10-07 12:16:45 -0400

    Time: 28 ms (97.66%), Space: 16.2 MB (57.77%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -8,5 +8,13 @@ class Solution:
             i += 1; j += 1
         res += word1[i:] if i < len(word1) else word2[j:]
         return res
+        # i, j = 0, 0
+        # res = ""
+        # while i < len(word1) and j < len(word2):
+        #     res += word1[i]
+        #     res += word2[j]
+        #     i += 1; j += 1
+        # res += word1[i:] if i < len(word1) else word2[j:]
+        # return res
 
         
