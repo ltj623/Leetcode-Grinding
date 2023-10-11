d8790f814bf6ddce53b5771d53b717d0c080b570
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-10-11 17:28:53 -0400

    Time: 46 ms (22.06%), Space: 16.3 MB (97.83%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -5,8 +5,7 @@ class Solution:
         return res
 
     def dfs(self, nums, index, path, res):
-        res.append(path)
-
         for i in range(index, len(nums)):
             self.dfs(nums, i+1, path + [nums[i]], res)
+        res.append(path)
 
