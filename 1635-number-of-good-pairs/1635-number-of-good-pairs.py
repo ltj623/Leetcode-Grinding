817c1044d4e60b6b6e7c2692df1f67a29cb9329a
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-10-18 11:27:58 -0400

    Time: 50 ms (5.45%), Space: 16.2 MB (71.21%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.py
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.py
@@ -2,11 +2,8 @@ class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         h = {}
         res = 0
-
-        for i, num in enumerate(nums):
-            if num in h:
-                res += h[num]
-                h[num] += 1
-            else:
-                h[num] = 1
+        for num in nums:
+            res += h.get(num,0)
+            h[num] = h.get(num,0) + 1
         return res
+        
