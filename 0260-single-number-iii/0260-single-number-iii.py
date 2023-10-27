2cc86f2ac4e476b63f4a9bd021a72e223570f1c5
Tingjie Liu <35505176+ltj623@users.noreply.github.com>
2023-10-27 17:12:18 -0400

    Time: 3464 ms (5.76%), Space: 18 MB (81.11%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,8 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
+        # res = []
+        # for num, time in Counter(nums).items():
+        #     if time == 1:
+        #         res.append(num)
+        # return res
         res = []
-        for num, time in Counter(nums).items():
-            if time == 1:
+        for num in nums:
+            if nums.count(num) == 1:
                 res.append(num)
         return res
         
